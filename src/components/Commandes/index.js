86d3419bcf470eafab7784bd5a6a6d705b4cc745
sopranoelfuego <aubinjaja@gmail.com>
2021-07-01 14:52:58 +0200

    se

--- src/components/Commandes/index.js
+++ src/components/Commandes/index.js
@@ -1,180 +1,173 @@
-import React, {useState,useEffect, useContext} from 'react';
-import {FirebaseContext } from '../Firebase';
+import React, { useState, useEffect, useContext } from 'react'
+import { FirebaseContext } from '../Firebase'
 import Adress from '../Adress'
 import ModalConfirm from '../ModalConfirm'
-import { v4 as uuidv4 } from 'uuid';
-import dateformat from "dateformat"
-const Commandes = (props) => {
+import { v4 as uuidv4 } from 'uuid'
+import dateformat from 'dateformat'
+const Commandes = props => {
+ const [openModal, setOpenModal] = useState(false)
 
-const [openModal, setOpenModal] = useState(false)
+ const { id, service } = props.match.params
+ const [currentUser, setCurrentUser] = useState()
+ const [idAdress, setIdAdress] = useState('')
+ const [track, setTrack] = useState(false)
+ const [unikId, setUnikId] = useState(uuidv4())
+ const [isUpdate, setIsUpdate] = useState('')
 
-const {id,service}=props.match.params
-const [currentUser, setCurrentUser] = useState()
-const [idAdress, setIdAdress] = useState("")
-const [track, setTrack] = useState(false)
-const [unikId, setUnikId] = useState(uuidv4())
-const [isUpdate,setIsUpdate]=useState("")
-console.log("this is the id and service",id,service)
-useEffect(() => {
-    setCurrentUser(firebase.auth.currentUser)
-}, [id])
-// currenteUser.uid
-const dataCommandes={
-    exigences: "",
-    idUser: "",
-    idEmployeMenage: "",
-    dateArriver:""
+ useEffect(() => {
+  setCurrentUser(firebase.auth.currentUser)
+ }, [id])
+ // currenteUser.uid
+ const dataCommandes = {
+  exigences: '',
+  idUser: '',
+  idEmployeMenage: '',
+  dateArriver: '',
+ }
+ const [commande, setCommande] = useState(dataCommandes)
+ const firebase = useContext(FirebaseContext)
 
+ const handleChange = e => {
+  setCommande({ ...commande, [e.target.id]: e.target.value })
+ }
 
+ // FONCTION QUI ACTIVE
+ const valide = (id, champ) => {
+  firebase.db
+   .collection(`${champ}`)
+   .doc(`${id}`)
+   .update({ isActive: true })
+   .then(data => console.log('mise a jour'))
+ }
 
+ const handleSubmit = e => {
+  e.preventDefault()
+  const { dateArriver, exigences, date } = commande
+
+  firebase.db
+   .collection('Commandes')
+   .where('idUser', '==', currentUser.uid)
+   .get()
+   .then(querySnapshot => {
+    querySnapshot.forEach(doc => {
+     // doc.data() is never undefined for query doc snapshots
+    })
+   })
+   .catch(error => {
+    console.log('Error getting documents: ', error)
+   })
+
+  if (localStorage.getItem('client')) {
+   firebase.db
+    .collection('employesMenages')
+    .doc(`${id}`)
+    .get()
+    .then(doc => {
+     console.log('this is the employesMenage', doc.data())
+
+     firebase.db
+      .collection('Demmande_sur_place')
+      .add({
+       dateArriver: dateArriver,
+       exigences: exigences,
+       idEmployeMenage: id,
+       fraisComission: Math.ceil(parseInt(doc.data().salaire, 10) / 10),
+       idClient_sur_place: localStorage.getItem('client'),
+       date: dateformat(new Date()),
+      })
+      .then(doc => {
+       valide(id, 'employesMenages')
+       localStorage.removeItem('client')
+       console.log('commande physic ajouter', doc.doc)
+      })
+      .catch(err => console.log('error qcure', err.message))
+    })
+  } else {
+   firebase.db
+    .collection('employesMenages')
+    .doc(`${id}`)
+    .get()
+    .then(doc => {
+     console.log('this is the employesMenage', doc.data())
+
+     firebase.db
+      .collection('Commandes')
+      .doc(`${unikId}`)
+      .set({
+       uuid: unikId,
+       dateArriver: dateArriver,
+       exigences: exigences,
+       idUser: currentUser.uid,
+       fraisComission: Math.ceil(parseInt(doc.data().salaire, 10) / 10),
+       idEmployeMenage: id,
+       date: dateformat(new Date()),
+      })
+      .then(doc => valide(id, 'employesMenages'))
+      .catch(error => console.log('error acure', error.message))
+    })
+  }
+ }
+
+ const closeModal = () => {
+  setOpenModal(false)
+ }
+
+ const { Commandes, exigences, date } = commande
+
+ return (
+  <>
+   {!track && (
+    <div className="signUpLoginBox">
+     <div className="slContainer">
+      <div className="formSignup">
+       <form onSubmit={handleSubmit}>
+        <h2>
+         <strong> PASSER VOTRE DEMANDE </strong>
+        </h2>
+
+        <div className="inputBox">
+         <label htmlFor="text">
+          Date de présentation de l'employé de menage
+         </label>
+         <input onChange={handleChange} type="date" id="dateArriver" />
+        </div>
+
+        <div className="inputBox">
+         <label htmlFor="text"> Vos exigences</label>
+         <textarea
+          onChange={handleChange}
+          type="textarea"
+          id="exigences"
+          placeholder="La description du profil de personnel de menage"
+         ></textarea>
+        </div>
+
+        <p>
+         Veillez verrifier vos informations avant de confirmer votre demande
+         d'employer de menage chez Housework
+        </p>
+
+        <button type="submit"> Confirmer </button>
+        <button
+         onClick={() => {
+          setTrack(true)
+         }}
+        >
+         {' '}
+         changer votre adresse{' '}
+        </button>
+       </form>
+      </div>
+     </div>
+    </div>
+   )}
+   {track && (
+    <Adress id={unikId} champ="Commandes" isUpdate={currentUser.uid} />
+   )}
+
+   {/* -- modal pop up  */}
+   <ModalConfirm showModal={openModal} closeModal={closeModal} />
+  </>
+ )
 }
-    const [commande, setCommande] = useState(dataCommandes);
-    const firebase = useContext(FirebaseContext);
-
-    const handleChange=(e)=>{
-
-        setCommande({...commande, [e.target.id]: e.target.value });
-    }
-    
-
-    // FONCTION QUI ACTIVE 
-    const  valide=(id,champ)=>{
-      firebase.db.collection(`${champ}`).doc(`${id}`).update({isActive:true}).then(
-      data=>console.log("mise a jour"))}
-   
-   
-   
-    const handleSubmit =(e)=>{
-        
-        e.preventDefault();
-        const {  dateArriver, exigences,date }=commande
-
-       firebase.db.collection("Commandes").where("idUser", "==", currentUser.uid)
-       .get()
-       .then((querySnapshot) => {
-           querySnapshot.forEach((doc) => {
-               // doc.data() is never undefined for query doc snapshots
-               
-           });
-       })
-       .catch((error) => {
-           console.log("Error getting documents: ", error);
-       });
-
-     if(localStorage.getItem("client")){
-
-    firebase.db.collection("employesMenages").doc(`${id}`).get().then(
-        doc=>{
-            console.log("this is the employesMenage",doc.data());
-            
-                   firebase.db.collection("Demmande_sur_place").add({
-                    dateArriver:dateArriver,
-                    exigences:exigences,
-                    idEmployeMenage:id,
-                    fraisComission : Math.ceil(parseInt(doc.data().salaire, 10)/10),
-                    idClient_sur_place:localStorage.getItem("client"),
-                    date:dateformat(new Date())
-            
-                   }).then(doc => {
-            
-                     valide(id,"employesMenages")
-                    localStorage.removeItem("client")
-                     console.log("commande physic ajouter",doc.doc)
-                   }
-                    ).catch(err=>console.log("error qcure",err.message))
-            
-        }
-      
-    )
-
-
-
-     }else{
-
-
-
-
-          
-    firebase.db.collection("employesMenages").doc(`${id}`).get().then(
-        doc=>{
-            console.log("this is the employesMenage",doc.data());
-            
-            
-                   firebase.db.collection('Commandes').doc(`${unikId}`).set({
-                      uuid:unikId,
-                      dateArriver:dateArriver,
-                     exigences:exigences,
-                      idUser:currentUser.uid,
-                    fraisComission : Math.ceil(parseInt(doc.data().salaire, 10)/10),
-                     idEmployeMenage:id,
-                     date:dateformat(new Date())
-              
-            
-                  }).then(doc => valide(id,"employesMenages")).catch(error=>console.log("error acure",error.message))
-
-            
-        }    
-      
-    )        
-
-     }
-
-   
-         
-       }
-       
-      const closeModal= ()=>{
-        setOpenModal(false);
-      }
-
-      const { Commandes, exigences, date}= commande
-    
-    return (
-
-        <>
-        {!track && <div className="signUpLoginBox">
-            <div className="slContainer">
-                   <div className="formSignup">
-
-                    <form onSubmit={handleSubmit}>
-                            <h2><strong> PASSER VOTRE DEMANDE </strong></h2>
-
-                            
-                            <div className="inputBox">
-                                <label htmlFor="text">Date de présentation de l'employé de menage</label>
-                                <input onChange={handleChange}   type="date" id="dateArriver"  />
-                            </div>
-
-                            <div className="inputBox">
-                                <label htmlFor="text"> Vos exigences</label>
-                                <textarea onChange={handleChange}  type="textarea"  id="exigences" placeholder="La description du profil de personnel de menage">
-                                    
-                                </textarea>
-                            </div>
-
-                            <p>Veillez verrifier vos informations avant de confirmer votre demande d'employer de menage chez Housework</p>
-
-                            <button type='submit' > Confirmer </button>
-                            <button onClick={()=>{
-
-                                setTrack(true)
-
-                            }} > changer votre adresse </button>
-                        </form>
-                   </div>
-
-               </div>
-            </div>}
-            {track && <Adress id={unikId} champ="Commandes" isUpdate={currentUser.uid}/>}
-
-
-          
-            {/* -- modal pop up  */}
-            <ModalConfirm showModal={openModal} closeModal={closeModal}/>
-
-
-                
-        </>
-    );
-}
-export default Commandes;
+export default Commandes
--- src/components/Tables/index.js
+++ src/components/Tables/index.js
@@ -1,137 +1,229 @@
-import React, {Fragment,useEffect,useState} from 'react';
-import {Link} from 'react-router-dom'
-import Modal from '../Modal';
+import React, { Fragment, useEffect, useState, useContext } from 'react'
+import { Link } from 'react-router-dom'
+import Modal from '../Modal'
+import { FirebaseContext } from '../Firebase'
 
 const Tables = () => {
-  
+ const firebase = useContext(FirebaseContext)
+ const [commandes, setCommandes] = useState([])
 
-  const [commandes,setCommandes]=useState([])
-  const [openModal, setOpenModal] = useState(false)
+ useEffect(() => {
+  console.log('entre de dans')
 
-  const showModal= id =>{
-    setOpenModal(true);
-  }
+  let container = []
+  firebase.db
+   .collection('Commandes')
+   .get()
+   .then(querySnapshot => {
+    querySnapshot.forEach(doc => {
+     // doc.data() is never undefined for query doc snapshots
+     container.push(doc.data())
+     //  console.log('document container', ' => ', container)
+    })
+   })
+   .then(() => {
+    for (let i = 0; i < container.length; i++) {
+     console.log(container[i].idEmployeMenage)
+     firebase.db
+      .collection('employesMenages')
+      .doc(`${container[i].idEmployeMenage}`)
+      .get()
+      .then(query => {
+       //  console.log('first this is the employesMenages object', query.data().nom)
+       //  container.push()
+       container[i].employe = query.data().nom
+      })
 
-  const closeModal= ()=>{
-    setOpenModal(false);
-  }
+     // CHECK NOW FOR THE USERS
+     firebase.db
+      .collection('users')
+      .doc(`${container[i].idUser}`)
+      .get()
+      .then(query => {
+       //  console.log('first this is the employesMenages object', query.data().nom)
+       //  container.push()
+       container[i].user = query.data().prenom
+      })
+     //  console.log('second this is the container[i].nom', container[i])
+    }
+   })
+   .then(() => {
+    setCommandes(container)
 
-  const details ="Details"
+    return container
+   })
+   .catch(err => console.log(err.message))
+ }, [])
 
-    return (
-<Fragment>
- <div className="">
+ commandes && console.log('commandes out here', commandes)
+ const [openModal, setOpenModal] = useState(false)
 
- </div>
+ const showModal = id => {
+  setOpenModal(true)
+ }
 
-<table className="tableau_style">
-  <thead>
-    <tr>
-        <th>Id_Commande</th>
-        <th>Client</th>
-        <th>Date</th>
-        <th>Présentation</th>
-        <th>Id_Adresse</th>
-        <th>Comission</th>
-        <th>E. menage</th>
-        <th>Détails</th>
-        <th>Valider</th>
-        <th>Edit</th>
-        <th>Delete</th>
-        
+ const closeModal = () => {
+  setOpenModal(false)
+ }
 
-      </tr>
-  </thead>
-      
-<tbody>
-  <tr>
-        <td>5vkjhckskjcskc</td>
-        <td>Luendo</td>
-        <td>2021-02-03</td>
-        <td>2021-02-05</td>
-        <td>Ngagara</td>
-        <td>10000</td>
-        <td>Iradukunda</td>
-        <td><Link onClick={()=>showModal(details)}>Voir plus</Link></td>
-        <td><button type="button" class="btn btn-success">Valider</button></td>
-        <td><button type="button" class="btn btn-secondary">Edit</button></td>
-        <td><button type="button" class="btn btn-danger">Delete</button></td>
+ const details = 'Details'
 
-      </tr>
-      <tr>
-        <td>5vkjhckskjcskc</td>
-        <td>Luendo</td>
-        <td>2021-02-03</td>
-        <td>2021-02-05</td>
-        <td>Buyenzi</td>
-        <td>10000</td>
-        <td>Iradukunda</td>
-        <td><Link onClick={()=>showModal(details)}>Voir plus</Link></td>
-        <td><button type="button" class="btn btn-success">Valider</button></td>
-        <td><button type="button" class="btn btn-secondary">Edit</button></td>
-        <td><button type="button" class="btn btn-danger">Delete</button></td>
+ return (
+  <Fragment>
+   <div className=""></div>
 
-      </tr>
-      <tr>
-        <td>5vkjhckskjcskc</td>
-        <td>Luendo</td>
-        <td>2021-02-03</td>
-        <td>2021-02-05</td>
-        <td>Nyakabiga</td>
-        <td>10000</td>
-        <td>Iradukunda</td>
-        <td><Link onClick={()=>showModal(details)}>Voir plus</Link></td>
-        <td><button type="button" class="btn btn-success">Valider</button></td>
-        <td><button type="button" class="btn btn-secondary">Edit</button></td>
-        <td><button type="button" class="btn btn-danger">Delete</button></td>
+   <table className="tableau_style">
+    <thead>
+     <tr>
+      <th>Id_Commande</th>
+      <th>Client</th>
+      <th>Date</th>
+      <th>Présentation</th>
+      <th>Id_Adresse</th>
+      <th>Comission</th>
+      <th>E. menage</th>
+      <th>Détails</th>
+      <th>Valider</th>
+      <th>Edit</th>
+      <th>Delete</th>
+     </tr>
+    </thead>
 
-      </tr>
-      <tr>
-        <td>5vkjhckskjcskc</td>
-        <td>Luendo</td>
-        <td>2021-02-03</td>
-        <td>2021-02-05</td>
-        <td>Bwie</td>
-        <td>10000</td>
-        <td>Iradukunda</td>
-        <td><Link onClick={()=>showModal(details)}>Voir plus</Link></td>
-        <td><button type="button" class="btn btn-success">Valider</button></td>
-        <td><button type="button" className="btn btn-secondary">Edit</button></td>
-        <td><button type="button" className="btn btn-danger">Delete</button></td>
+    <tbody>
+     <tr>
+      <td>5vkjhckskjcskc</td>
+      <td>Luendo</td>
+      <td>2021-02-03</td>
+      <td>2021-02-05</td>
+      <td>Ngagara</td>
+      <td>10000</td>
+      <td>Iradukunda</td>
+      <td>
+       <Link onClick={() => showModal(details)}>Voir plus</Link>
+      </td>
+      <td>
+       <button type="button" class="btn btn-success">
+        Valider
+       </button>
+      </td>
+      <td>
+       <button type="button" class="btn btn-secondary">
+        Edit
+       </button>
+      </td>
+      <td>
+       <button type="button" class="btn btn-danger">
+        Delete
+       </button>
+      </td>
+     </tr>
+     <tr>
+      <td>5vkjhckskjcskc</td>
+      <td>Luendo</td>
+      <td>2021-02-03</td>
+      <td>2021-02-05</td>
+      <td>Buyenzi</td>
+      <td>10000</td>
+      <td>Iradukunda</td>
+      <td>
+       <Link onClick={() => showModal(details)}>Voir plus</Link>
+      </td>
+      <td>
+       <button type="button" class="btn btn-success">
+        Valider
+       </button>
+      </td>
+      <td>
+       <button type="button" class="btn btn-secondary">
+        Edit
+       </button>
+      </td>
+      <td>
+       <button type="button" class="btn btn-danger">
+        Delete
+       </button>
+      </td>
+     </tr>
+     <tr>
+      <td>5vkjhckskjcskc</td>
+      <td>Luendo</td>
+      <td>2021-02-03</td>
+      <td>2021-02-05</td>
+      <td>Nyakabiga</td>
+      <td>10000</td>
+      <td>Iradukunda</td>
+      <td>
+       <Link onClick={() => showModal(details)}>Voir plus</Link>
+      </td>
+      <td>
+       <button type="button" class="btn btn-success">
+        Valider
+       </button>
+      </td>
+      <td>
+       <button type="button" class="btn btn-secondary">
+        Edit
+       </button>
+      </td>
+      <td>
+       <button type="button" class="btn btn-danger">
+        Delete
+       </button>
+      </td>
+     </tr>
+     <tr>
+      <td>5vkjhckskjcskc</td>
+      <td>Luendo</td>
+      <td>2021-02-03</td>
+      <td>2021-02-05</td>
+      <td>Bwie</td>
+      <td>10000</td>
+      <td>Iradukunda</td>
+      <td>
+       <Link onClick={() => showModal(details)}>Voir plus</Link>
+      </td>
+      <td>
+       <button type="button" class="btn btn-success">
+        Valider
+       </button>
+      </td>
+      <td>
+       <button type="button" className="btn btn-secondary">
+        Edit
+       </button>
+      </td>
+      <td>
+       <button type="button" className="btn btn-danger">
+        Delete
+       </button>
+      </td>
+     </tr>
+    </tbody>
+   </table>
 
-      </tr>
-</tbody>
-      
+   <Modal showModal={openModal} closeModal={closeModal}>
+    <div className="modalHeader">
+     <h2>DESCRIPTION DU COMMANDE</h2>
+    </div>
 
-</table>
+    <div className="modalBody">
+     <div>
+      <p>dateArriver: le 6 juillet</p>
+      <p>exigences: il doit etre plus propre</p>
+      <p>EmployeMenage: Irakoze</p>
+      <p>fraisComission: 10000</p>
+      <p>Client_sur_place: Fidele</p>
+      <p>Client_sur_place: Fidele</p>
+      <p>date de Commande: Fidele</p>
+     </div>
+    </div>
 
-<Modal showModal={openModal} closeModal={closeModal}>
-                <div className="modalHeader">
-                    <h2>DESCRIPTION DU COMMANDE</h2>
-                </div>
+    <div className="modalFooter">
+     <button className="modalBtn">Fermer</button>
+    </div>
+   </Modal>
+  </Fragment>
+ )
+}
 
-                <div className="modalBody">
-                    <div>
-                    <p>dateArriver: le 6 juillet</p>
-                    <p>exigences: il doit etre plus propre</p>
-                    <p>EmployeMenage: Irakoze</p>
-                    <p>fraisComission: 10000</p>
-                    <p>Client_sur_place: Fidele</p>
-                    <p>Client_sur_place: Fidele</p>
-                    <p>date de Commande: Fidele</p>
-                    </div>
-                </div>
-
-                <div className="modalFooter">
-                    <button className="modalBtn">Fermer</button>
-                </div>
-            </Modal>
-
-
-</Fragment>
-
-
-    );
-};
-
-export default Tables;
+export default Tables
