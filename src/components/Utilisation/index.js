ab92fd1dbafa9329becf3fe94a7c5ca1c6ddfef5
Luendo <luendoglory8@gmail.com>
2021-07-02 10:52:55 +0200

    tables

--- src/components/Utilisation/index.js
+++ src/components/Utilisation/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 
 const Utilisation = () => {
     return (
         <div className="wrapper">
-            Comment ça marche
+            <h1 className="utilisation"> Guite d'utilisation</h1>
+            <h3>1. Authentification</h3>
+            <p>
+                Pour avoir accès aux services proposés par Housework vous devez cliquer sur le bouton
+                <strong ><Link to="/"> COMMENCER </Link></strong> se trouvant sur la page d'accueil pour vous connecter. <br />
+                Si vous n'avez pas un compte sur Housework vous devez créer un compte pour bien profiter de cette application. <br />
+                Si lors de votre dernière connexion vous n'étiez pas déconnecter, vous devez cliquer sur le menu <strong>Services</strong> en haut pour acceder directement aux services                           
+            </p>
         </div>
     );
 };
